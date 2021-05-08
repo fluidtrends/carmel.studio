87735f6a8beeb92359ca84187df8388c49f078af
I. Dan Calinescu <idancalinescu@gmail.com>
2021-05-08 16:20:15 +0300

    added base test framework

---
+++ test/specs/main.ts
@@ -0,0 +1,12 @@
+import savor, {
+    Context,
+    Completion
+} from 'savor'
+
+savor.
+
+add('should start a simple session', (context: Context, done: Completion) => {
+    done()
+}).
+
+run('[Carmel Studio] main')
